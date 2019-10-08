8049ff659ddbcf56447b84c88b7f23244fb30955
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2019-10-08 21:48:00 +0200

    cli: Refactor + read version from package.json
    TypeScript wasn't as keen on importing json files as Babel/Node.js is,
    so this commit reads package.json as a regular file and JSON decodes it.
    
    The main argument parsing code has been moved into main() to clean
    things up a bit.

--- src/cli.ts
+++ src/cli.ts
@@ -3,15 +3,18 @@
 import commander from 'commander';
 
 import * as fs from 'fs';
+import * as path from 'path';
 import * as REPL from 'repl';
 
-// this import messes up the package structure after running tsc
-// import * as pkg from '../package.json';
 import { connect } from './';
 import log from './log';
 import XAPI from './xapi/index.js';
 
-const pkg = { version: '4.2.0' };
+function readPkg() {
+  const filepath = path.join(__dirname, '..', 'package.json');
+  const data = fs.readFileSync(filepath, 'utf8');
+  return JSON.parse(data);
+}
 
 function evalFile(source: any, xapi: XAPI) {
   const context = new Function('xapi', source); // eslint-disable-line no-new-func
@@ -24,40 +27,45 @@ function startRepl(xapi: XAPI) {
   repl.context.xapi = xapi;
 }
 
-commander
-  .version(pkg.version)
-  .arguments('<host> [file]')
-  .description('connect to a codec and launch a repl')
-  .option('-p, --port <port>', 'port to connect to', 22)
-  .option('-U, --username <user>', 'username to authenticate with', 'admin')
-  .option('-P, --password <password>', 'password to authenticate with', '')
-  .option('-C, --command <command>', 'command to execute on remote host', '')
-  .option(
-    '-l, --loglevel <level>',
-    'set application log level (trace|debug|info|warn|error|silent)',
-    /^(trace|debug|info|warn|error|silent)$/i,
-    'warn',
-  )
-  .action((host, file, options) => {
-    if (!host) {
-      log.error('Please specify a host to connect to');
-      commander.help();
-    }
-
-    const source = file && fs.readFileSync(file);
-    const xapi = connect(
-      host,
-      options,
+function main() {
+  const { version } = readPkg();
+  commander
+    .version(version)
+    .arguments('<host> [file]')
+    .description('connect to a codec and launch a repl')
+    .option('-p, --port <port>', 'port to connect to', 22)
+    .option('-U, --username <user>', 'username to authenticate with', 'admin')
+    .option('-P, --password <password>', 'password to authenticate with', '')
+    .option('-C, --command <command>', 'command to execute on remote host', '')
+    .option(
+      '-l, --loglevel <level>',
+      'set application log level (trace|debug|info|warn|error|silent)',
+      /^(trace|debug|info|warn|error|silent)$/i,
+      'warn',
     )
-      .on('error', (error: any) => {
-        log.error('xapi error:', error);
-      })
-      .on('ready', () => {
-        if (source) {
-          evalFile(source, xapi);
-        } else {
-          startRepl(xapi);
-        }
-      });
-  })
-  .parse(process.argv);
+    .action((host, file, options) => {
+      if (!host) {
+        log.error('Please specify a host to connect to');
+        commander.help();
+      }
+
+      const source = file && fs.readFileSync(file);
+      const xapi = connect(
+        host,
+        options,
+      )
+        .on('error', (error: any) => {
+          log.error('xapi error:', error);
+        })
+        .on('ready', () => {
+          if (source) {
+            evalFile(source, xapi);
+          } else {
+            startRepl(xapi);
+          }
+        });
+    })
+    .parse(process.argv);
+}
+
+main();
