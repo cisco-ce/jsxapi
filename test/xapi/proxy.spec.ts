f30a97bd92abd90dc40dbaf04bb193d007c02881
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2019-12-23 23:58:44 +0100

    test: Port proxy tests to jest

--- test/xapi/proxy.spec.ts
+++ test/xapi/proxy.spec.ts
@@ -1,31 +1,28 @@
-import { expect } from 'chai';
-import sinon from 'sinon';
-
 import Backend from '../../src/backend';
 import XAPI from '../../src/xapi';
 
 describe('Proxy', () => {
-  let execute: (...args: any[]) => any;
+  let execute: jest.SpyInstance;
   let xapi: XAPI;
 
   beforeEach(() => {
     xapi = new XAPI(new Backend(), { seal: false });
-    execute = sinon.stub(xapi, 'execute');
+    execute = jest.spyOn(xapi, 'execute').mockImplementation(() => Promise.resolve({}));
   });
 
   describe('command', () => {
     it('can proxy xapi.command', () => {
       xapi.Command.Audio.Volume.Mute();
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xCommand/Audio/Volume/Mute');
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xCommand/Audio/Volume/Mute', undefined);
     });
 
     it('can proxy xapi.command with args', () => {
       xapi.Command.Dial({ Number: 'user@example.com' });
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xCommand/Dial', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xCommand/Dial', {
         Number: 'user@example.com',
       });
     });
@@ -35,8 +32,8 @@ describe('Proxy', () => {
     it('can proxy xapi.config..get()', () => {
       xapi.Config.Audio.DefaultVolume.get();
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xGet', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xGet', {
         Path: ['Configuration', 'Audio', 'DefaultVolume'],
       });
     });
@@ -44,8 +41,8 @@ describe('Proxy', () => {
     it('can proxy xapi.config..set()', () => {
       xapi.Config.Audio.DefaultVolume.set(50);
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xSet', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xSet', {
         Path: ['Configuration', 'Audio', 'DefaultVolume'],
         Value: 50,
       });
@@ -54,19 +51,19 @@ describe('Proxy', () => {
     it('can proxy xapi.config..set() with array index', () => {
       xapi.Config.FacilityService.Service[3].Number.set('user@example.com');
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xSet', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xSet', {
         Path: ['Configuration', 'FacilityService', 'Service', 3, 'Number'],
         Value: 'user@example.com',
       });
     });
 
     it('can proxy feedback registration xapi.config..on()', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       xapi.Config.Audio.DefaultVolume.on(spy);
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xFeedback/Subscribe', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xFeedback/Subscribe', {
         Query: ['Configuration', 'Audio', 'DefaultVolume'],
       });
     });
@@ -74,11 +71,11 @@ describe('Proxy', () => {
 
   describe('event', () => {
     it('can proxy feedback registration xapi.event..on()', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       xapi.Event.Foo.Bar.on(spy);
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xFeedback/Subscribe', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xFeedback/Subscribe', {
         Query: ['Event', 'Foo', 'Bar'],
       });
     });
@@ -88,8 +85,8 @@ describe('Proxy', () => {
     it('can proxy xapi.status..get()', () => {
       xapi.Status.Audio.Volume.get();
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xGet', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xGet', {
         Path: ['Status', 'Audio', 'Volume'],
       });
     });
@@ -97,18 +94,18 @@ describe('Proxy', () => {
     it('can proxy xapi.status..get() with array index', () => {
       xapi.Status.Video.Input.Connector[2].Type.get();
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xGet', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xGet', {
         Path: ['Status', 'Video', 'Input', 'Connector', 2, 'Type'],
       });
     });
 
     it('can proxy feedback registration xapi.status..on()', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       xapi.Status.Audio.Volume.on(spy);
 
-      expect(execute).to.have.been.calledOnce();
-      expect(execute).to.have.been.calledWith('xFeedback/Subscribe', {
+      expect(execute).toHaveBeenCalledTimes(1);
+      expect(execute).toHaveBeenCalledWith('xFeedback/Subscribe', {
         Query: ['Status', 'Audio', 'Volume'],
       });
     });
