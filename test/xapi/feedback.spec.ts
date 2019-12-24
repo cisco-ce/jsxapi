4bccc8ae26129aabf96961e751b9a69723646cc7
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2019-12-24 12:36:21 +0100

    test: Port feedback tests to jest

--- test/xapi/feedback.spec.ts
+++ test/xapi/feedback.spec.ts
@@ -1,6 +1,3 @@
-import { expect } from 'chai';
-import * as sinon from 'sinon';
-
 import Backend from '../../src/backend';
 import XAPI from '../../src/xapi';
 import Feedback, { FeedbackGroup } from '../../src/xapi/feedback';
@@ -17,58 +14,61 @@ function getPath(obj: any, ...path: any[]) {
 }
 
 describe('Feedback', () => {
-  let interceptor: sinon.SinonStub<any | undefined, void>;
+  let interceptor: jest.SpyInstance;
   let feedback: Feedback;
   let xapi: XAPI;
-  let executeStub: sinon.SinonStub<any>;
+  let executeStub: jest.SpyInstance;
 
   beforeEach(() => {
-    interceptor = sinon.stub();
-    interceptor.callsArg(1);
+    interceptor = jest.fn().mockImplementation((_, fn) => {
+      fn();
+    });
     xapi = new XAPI(new Backend(), {
       feedbackInterceptor: interceptor,
       seal: true,
-    });
+    } as any);
     ({ feedback } = xapi);
 
     let nextSubscriptionId = 0;
-    executeStub = sinon.stub(XAPI.prototype, 'execute').callsFake((method) => {
-      switch (method) {
-        case 'xFeedback/Subscribe': {
-          const id = nextSubscriptionId;
-          nextSubscriptionId += 1;
-          return Promise.resolve({ Id: id });
+    executeStub = jest
+      .spyOn(XAPI.prototype, 'execute')
+      .mockImplementation((method) => {
+        switch (method) {
+          case 'xFeedback/Subscribe': {
+            const id = nextSubscriptionId;
+            nextSubscriptionId += 1;
+            return Promise.resolve({ Id: id });
+          }
+          default:
+            return Promise.resolve({ Id: 52 });
         }
-        default:
-          return Promise.resolve({ Id: 52 });
-      }
-    });
+      });
   });
 
   afterEach(() => {
-    executeStub.restore();
-  })
+    executeStub.mockRestore();
+  });
 
   it('is instance of Feedback', () => {
-    expect(feedback).to.be.instanceof(Feedback);
+    expect(feedback).toBeInstanceOf(Feedback);
   });
 
   describe('.dispatch()', () => {
     it('returns this', () => {
-      expect(feedback.dispatch({ Status: 'foo' })).to.equal(feedback);
+      expect(feedback.dispatch({ Status: 'foo' })).toEqual(feedback);
     });
 
     it('fires event', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       feedback.on('Status', spy);
       feedback.dispatch({ Status: 'foo' });
-      expect(spy).to.have.been.calledOnce();
-      expect(spy).to.have.been.calledWith('foo');
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith('foo', expect.anything());
     });
 
     it('fires events recursively', () => {
       const data = { Status: { Audio: { Volume: '50' } } };
-      const spies = [sinon.spy(), sinon.spy(), sinon.spy(), sinon.spy()];
+      const spies = [jest.fn(), jest.fn(), jest.fn(), jest.fn()];
 
       feedback.on('', spies[0]);
       feedback.on('Status', spies[1]);
@@ -78,82 +78,79 @@ describe('Feedback', () => {
 
       [data, data.Status, data.Status.Audio, data.Status.Audio.Volume].forEach(
         (d, i) => {
-          expect(spies[i].firstCall).to.have.been.calledWith(d);
+          expect(spies[i]).toHaveBeenNthCalledWith(1, d, expect.anything());
         },
       );
     });
 
     it('does not invoke unrelated handlers', () => {
-      const spy1 = sinon.spy();
-      const spy2 = sinon.spy();
+      const spy1 = jest.fn();
+      const spy2 = jest.fn();
 
       feedback.on('Status/Audio/Volume', spy1);
       feedback.on('Status/Audio/VolumeMute', spy2);
       feedback.dispatch({ Status: { Audio: { VolumeMute: 'off' } } });
 
-      expect(spy1).to.not.have.been.called();
-      expect(spy2).to.have.been.calledOnce();
-      expect(spy2).to.have.been.calledWith('off');
+      expect(spy1).not.toHaveBeenCalled();
+      expect(spy2).toHaveBeenCalledTimes(1);
+      expect(spy2).toHaveBeenCalledWith('off', expect.anything());
     });
 
     it('dispatches original feedback payload as second argument', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       const data = { Status: { Call: [{ id: 42, Status: 'Connected' }] } };
 
       feedback.on('Status/Call/Status', spy);
       feedback.dispatch(data);
 
-      expect(spy).to.have.been.calledWith('Connected', data);
+      expect(spy).toHaveBeenCalledWith('Connected', data);
     });
 
     it('can listen to lower-case events', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       const data = { Status: { Call: [{ id: 42, ghost: 'True' }] } };
 
       feedback.on('Status/Call/ghost', spy);
       feedback.dispatch(data);
 
-      expect(spy).to.have.been.calledWith('True', data);
+      expect(spy).toHaveBeenCalledWith('True', data);
     });
   });
 
   describe('.on()', () => {
     it('registers handler for events', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       feedback.on('Status/Audio/Volume', spy);
       feedback.dispatch({ Status: { Audio: { Volume: 50 } } });
 
-      expect(spy).to.have.been.calledWith(50);
+      expect(spy).toHaveBeenCalledWith(50, expect.anything());
     });
 
     it('returns handler for disabling feedback', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       const handler = feedback.on('Status/Audio/Volume', spy);
       feedback.dispatch({ Status: { Audio: { Volume: 50 } } });
 
-      expect(spy).to.have.been.calledWith(50);
-      spy.resetHistory();
+      expect(spy).toHaveBeenCalledWith(50, expect.anything());
+      spy.mockClear();
 
       handler();
 
       feedback.dispatch({ Status: { Audio: { Volume: 50 } } });
-      expect(spy).to.not.have.been.called();
+      expect(spy).not.toHaveBeenCalled();
     });
 
     it('off handler contains registration promise', async () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       const regs = await Promise.all([
         feedback.on('Status/Audio/Volume', spy).registration,
         feedback.on('Status/Audio/Volume', spy).registration,
       ]);
 
-      expect(regs).to.deep.equal([
-        { Id: 0 },
-        { Id: 1 },
-      ]);
+      expect(regs).toEqual([{ Id: 0 }, { Id: 1 }]);
     });
 
     it('registers feedback with the backend', () => {
@@ -161,13 +158,13 @@ describe('Feedback', () => {
 
       feedback.on(path, () => {});
 
-      expect(xapi.execute).to.have.been.calledWith('xFeedback/Subscribe', {
+      expect(xapi.execute).toHaveBeenCalledWith('xFeedback/Subscribe', {
         Query: ['Status', 'Audio', 'Volume'],
       });
     });
 
     it('cancelling double registration leaves one listener', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       const path = 'Status/Audio/Volume';
 
       feedback.on(path, spy);
@@ -176,8 +173,8 @@ describe('Feedback', () => {
 
       feedback.dispatch({ Status: { Audio: { Volume: 50 } } });
 
-      expect(spy).to.have.been.calledOnce();
-      expect(spy).to.have.been.calledWith(50);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(50, expect.anything());
     });
 
     it('normalizes path', () => {
@@ -185,34 +182,34 @@ describe('Feedback', () => {
 
       feedback.on(path, () => {});
 
-      expect(xapi.execute).to.have.been.calledWith('xFeedback/Subscribe', {
+      expect(xapi.execute).toHaveBeenCalledWith('xFeedback/Subscribe', {
         Query: ['Status', 'Audio', 'Volume'],
       });
     });
 
     it('can dispatch to normalized path', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       feedback.on('status/audio   volume', spy);
       feedback.dispatch({ Status: { Audio: { Volume: 50 } } });
 
-      expect(spy.firstCall).to.have.been.calledWith(50);
+      expect(spy).toHaveBeenNthCalledWith(1, 50, expect.anything());
     });
 
     it('can use crazy casing', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       feedback.on('fOO Bar BaZ', spy);
       feedback.dispatch({ Foo: { Bar: { Baz: 50 } } });
 
-      expect(spy.firstCall).to.have.been.calledWith(50);
+      expect(spy).toHaveBeenNthCalledWith(1, 50, expect.anything());
     });
 
     it('handles arrays', () => {
-      const spy1 = sinon.spy();
-      const spy2 = sinon.spy();
-      const spy3 = sinon.spy();
-      const spy4 = sinon.spy();
+      const spy1 = jest.fn();
+      const spy2 = jest.fn();
+      const spy3 = jest.fn();
+      const spy4 = jest.fn();
 
       feedback.on('Status/Peripherals/ConnectedDevice/Status', spy1);
       feedback.on('Status/Peripherals/ConnectedDevice[]/Status', spy2);
@@ -229,25 +226,23 @@ describe('Feedback', () => {
         },
       });
 
-      expect(spy1).to.have.been.calledTwice();
-      expect(spy1).to.have.been.calledWith('LostConnection');
-      expect(spy1).to.have.been.calledWith('Connected');
+      expect(spy1).toHaveBeenNthCalledWith(1, 'LostConnection', expect.anything());
+      expect(spy1).toHaveBeenNthCalledWith(2, 'Connected', expect.anything());
 
-      expect(spy2).to.have.been.calledTwice();
-      expect(spy2).to.have.been.calledWith('LostConnection');
-      expect(spy2).to.have.been.calledWith('Connected');
+      expect(spy2).toHaveBeenNthCalledWith(1, 'LostConnection', expect.anything());
+      expect(spy2).toHaveBeenNthCalledWith(2, 'Connected', expect.anything());
 
-      expect(spy3).to.have.been.calledOnce();
-      expect(spy3).to.have.been.calledWith('LostConnection');
-      expect(spy3).to.not.have.been.calledWith('Connected');
+      expect(spy3).toHaveBeenCalledTimes(1);
+      expect(spy3).toHaveBeenCalledWith('LostConnection', expect.anything());
+      expect(spy3).not.toHaveBeenCalledWith('Connected', expect.anything());
 
-      expect(spy4).to.have.been.calledOnce();
-      expect(spy4).to.have.been.calledWith('LostConnection');
-      expect(spy4).to.not.have.been.calledWith('Connected');
+      expect(spy4).toHaveBeenCalledTimes(1);
+      expect(spy4).toHaveBeenCalledWith('LostConnection', expect.anything());
+      expect(spy4).not.toHaveBeenCalledWith('Connected', expect.anything());
     });
 
     it('dispatches array elements one-by-one', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       feedback.on('foo/bar', spy);
 
@@ -255,12 +250,12 @@ describe('Feedback', () => {
         foo: { bar: [{ baz: 'quux' }] },
       });
 
-      expect(spy).to.have.been.calledOnce();
-      expect(spy).to.have.been.calledWith({ baz: 'quux' });
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith({ baz: 'quux' }, expect.anything());
     });
 
     it('handles ghost events', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       feedback.on('Status/Peripherals/ConnectedDevice', spy);
       feedback.dispatch({
@@ -276,42 +271,46 @@ describe('Feedback', () => {
         },
       });
 
-      expect(spy.firstCall).to.have.been.calledWith({
-        id: '1115',
-        ghost: 'True',
-      });
+      expect(spy).toHaveBeenNthCalledWith(
+        1,
+        {
+          id: '1115',
+          ghost: 'True',
+        },
+        expect.anything(),
+      );
     });
 
     it('is called by .once()', () => {
       const path = 'Status/Audio/Volume';
       const listener = () => {};
 
-      const spy = sinon.spy(feedback, 'on');
+      const spy = jest.spyOn(feedback, 'on');
       feedback.once(path, listener);
 
-      const [callPath, callListener] = spy.firstCall.args;
+      const [callPath, callListener] = spy.mock.calls[0];
 
-      expect(callPath).to.equal(path);
-      expect((callListener as any).listener).to.equal(listener);
+      expect(callPath).toEqual(path);
+      expect((callListener as any).listener).toEqual(listener);
     });
   });
 
   describe('.once()', () => {
     it('deregisters after emit', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       feedback.once('Status/Audio/Volume', spy);
       feedback.dispatch({ Status: { Audio: { Volume: '50' } } });
       feedback.dispatch({ Status: { Audio: { Volume: '70' } } });
 
-      expect(spy).to.have.been.calledWith('50');
-      expect(spy).to.not.have.been.calledWith('70');
+      expect(spy).toHaveBeenCalledWith('50', expect.anything());
+      expect(spy).not.toHaveBeenCalledWith('70', expect.anything());
     });
   });
 
   describe('.off()', () => {
     it('is now deprecated/removed and throws Error', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
       const path = 'Status/Audio/Volume';
 
       feedback.on(path, spy);
@@ -320,61 +319,66 @@ describe('Feedback', () => {
         feedback.off();
       };
 
-      expect(off).to.throw(Error, /deprecated/);
+      expect(off).toThrow(/deprecated/);
     });
   });
 
   describe('interceptor', () => {
     beforeEach(() => {
-      interceptor.reset();
+      interceptor.mockReset();
     });
 
     it('can reject feedback', () => {
-      const volumeSpy = sinon.spy();
+      const volumeSpy = jest.fn();
       const data = { Status: { Audio: { Volume: '50' } } };
 
-      interceptor.onFirstCall().callsFake(() => {});
-      interceptor.onSecondCall().callsArg(1);
+      interceptor
+        .mockImplementationOnce(() => {})
+        .mockImplementationOnce((_, fn) => {
+          fn();
+        });
 
       feedback.on('Status Audio Volume', volumeSpy);
 
       feedback.dispatch(data);
-      expect(volumeSpy).to.not.have.been.called();
+      expect(volumeSpy).not.toHaveBeenCalled();
 
       feedback.dispatch(data);
-      expect(volumeSpy).to.have.been.calledOnce();
-      expect(volumeSpy).to.have.been.calledWith('50', data);
+      expect(volumeSpy).toHaveBeenCalledTimes(1);
+      expect(volumeSpy).toHaveBeenCalledWith('50', data);
     });
 
     it('can change the data', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
-      interceptor.callsFake((data: any, dispatch: (d: any) => void) => {
-        const item = getPath(data, 'Status', 'Audio', 'Volume');
-        if (item) {
-          data.Status.Audio.Volume = '100';
-          dispatch(data);
-        }
-      });
+      interceptor.mockImplementation(
+        (data: any, dispatch: (d: any) => void) => {
+          const item = getPath(data, 'Status', 'Audio', 'Volume');
+          if (item) {
+            data.Status.Audio.Volume = '100';
+            dispatch(data);
+          }
+        },
+      );
 
       feedback.on('Status Audio Volume', spy);
 
       const data = { Status: { Audio: { Volume: '50' } } };
       feedback.dispatch(data);
 
-      expect(spy).to.have.been.calledOnce();
-      expect(spy).to.have.been.calledWith('100', data);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith('100', data);
     });
   });
 
   describe('.group()', () => {
     let group: FeedbackGroup;
-    let muteSpy: sinon.SinonSpy;
-    let volumeSpy: sinon.SinonSpy;
+    let muteSpy: jest.Mock;
+    let volumeSpy: jest.Mock;
 
     beforeEach(() => {
-      muteSpy = sinon.spy();
-      volumeSpy = sinon.spy();
+      muteSpy = jest.fn();
+      volumeSpy = jest.fn();
       group = feedback.group([
         xapi.status.on('Audio/Volume', volumeSpy),
         xapi.status.on('Audio/VolumeMute', muteSpy),
@@ -391,12 +395,12 @@ describe('Feedback', () => {
         },
       });
 
-      expect(volumeSpy.firstCall).to.have.been.calledWith('50');
-      expect(muteSpy.firstCall).to.have.been.calledWith('On');
+      expect(volumeSpy).toHaveBeenNthCalledWith(1, '50', expect.anything());
+      expect(muteSpy).toHaveBeenNthCalledWith(1, 'On', expect.anything());
     });
 
     it('only deregisters feedback of the group', () => {
-      const rootSpy = sinon.spy();
+      const rootSpy = jest.fn();
       xapi.status.on('Audio/Volume', rootSpy);
 
       group.off();
@@ -410,13 +414,13 @@ describe('Feedback', () => {
         },
       });
 
-      expect(rootSpy.firstCall).to.have.been.calledWith('50');
-      expect(volumeSpy).to.not.have.been.called();
-      expect(muteSpy).to.not.have.been.called();
+      expect(rootSpy).toHaveBeenNthCalledWith(1, '50', expect.anything());
+      expect(volumeSpy).not.toHaveBeenCalled();
+      expect(muteSpy).not.toHaveBeenCalled();
     });
 
     it('supports .once()', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       group.off();
       group.add(xapi.status.once('Audio/Volume', spy));
@@ -424,19 +428,19 @@ describe('Feedback', () => {
       feedback.dispatch({ Status: { Audio: { Volume: '50' } } });
       feedback.dispatch({ Status: { Audio: { Volume: '70' } } });
 
-      expect(spy).to.have.been.calledOnce();
-      expect(spy.firstCall).to.have.been.calledWith('50');
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith('50', expect.anything());
     });
 
     it('can clean up .once() before emit with .off()', () => {
-      const spy = sinon.spy();
+      const spy = jest.fn();
 
       group.add(xapi.status.once('Status/Audio/Volume', spy));
       group.off();
 
       feedback.dispatch({ Status: { Audio: { Volume: '50' } } });
 
-      expect(spy).to.not.have.been.called();
+      expect(spy).not.toHaveBeenCalled();
     });
   });
 });
