cc7ca6993f7952a30a8b1229343200ba0b26bbe3
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2020-03-16 21:37:05 +0100

    5.0.3

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 /*
  * DO NOT WRITE OR UPDATE THIS FILE!
- * This file was automatically generated at: 2020-03-16T20:05:40.388Z
+ * This file was automatically generated at: 2020-03-16T20:36:47.280Z
  */
-const VERSION = '5.0.2';
+const VERSION = '5.0.3';
 export default VERSION;
