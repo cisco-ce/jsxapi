94c735da4d6f062c51b2c2e5ddf6170edabc9948
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2020-04-27 09:19:51 +0200

    ws: Allow overriding port

--- src/transport/ws.ts
+++ src/transport/ws.ts
@@ -20,11 +20,12 @@ function generateAuthSubProto(username: string, password: string): string {
 }
 
 export default function websocketConnect(createWebSocket: CreateWebSocket, opts: Options) {
-  const { host, username, password, protocol } = opts;
+  const { host, username, password, port, protocol } = opts;
   const url = new Url('');
   url.set('pathname', '/ws');
   url.set('host', host);
   url.set('protocol', protocol);
+  url.set('port', port);
 
   const auth = generateAuthSubProto(username, password);
   return createWebSocket(url.href, auth);
