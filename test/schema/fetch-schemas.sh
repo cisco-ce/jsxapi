ccb140bcfa725a36cc4e62e2433c5c57a6ca1aea
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2020-04-29 15:53:01 +0200

    schema: Add format parameter to fetcher

--- test/schema/fetch-schemas.sh
+++ test/schema/fetch-schemas.sh
@@ -3,22 +3,25 @@
 set -e
 
 if [ "$#" -lt 1 ]; then
-    echo "usage: fetch-schemas hostname" >&2
+    echo "usage: fetch-schemas hostname [format]" >&2
+    echo "  format: 'json' (default) or 'xml'" >&2
     exit 1
 fi
+
+fmt=${2:-json}
 
 get_schema() {
     # "xpref outputmode json" - to avoid printing "OK" at the end
     # "xpref accessmode internal" - because xdoc is internal
-    # piping to sed drops all lines until the opening "{" - skip welcome text
-    ssh "$1" <<EOF | sed -n '/{/,$p'
-xpref outputmode json
+    # piping to sed drops all lines until the first "OK" - skip welcome text
+    ssh -T "$1" <<EOF | sed -n '/^OK$/,$p' | tail +3
+xpref outputmode "$fmt"
 xpref accessmode internal
-xdoc path: "$2" schema: true format: json
+xdoc Path: "$2" Schema: true Format: "$fmt"
 EOF
 }
 
 for doc in command config status; do
-    echo "Fetching '$doc'"
-    get_schema "$1" "$doc" > "$doc.json"
+    echo "Fetching '$doc.$fmt'"
+    get_schema "$1" "$doc" > "$doc.$fmt"
 done
