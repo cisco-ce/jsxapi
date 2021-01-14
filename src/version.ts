b04a029a77c3666d24630bf384bed8c742ebd17a
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2021-01-14 09:46:20 +0100

    5.1.0-1

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 /*
  * DO NOT WRITE OR UPDATE THIS FILE!
- * This file was automatically generated at: 2020-08-14T08:56:40.162Z
+ * This file was automatically generated at: 2021-01-14T08:46:03.752Z
  */
-const VERSION = '5.1.0-0';
+const VERSION = '5.1.0-1';
 export default VERSION;
